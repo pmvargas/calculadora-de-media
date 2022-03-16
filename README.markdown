1354666ad85535e4c3a9e8be770a3bdda831f6b3
Paula Vargas <paulavargas-sm@hotmail.com>
2022-03-16 16:51:57 -0300

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/pmvargas/pen/eYypvvX](https://codepen.io/pmvargas/pen/eYypvvX).
+
+
---
+++ src/script.js
@@ -0,0 +1,68 @@
+var nome ="Quadro de Notas da aluna Paula Vargas do Curso Superior de Tecnologia de Geoprocessamento"
+
+// Notas de BANCO DE DADOS ESPACIAIS II
+var notaDoPrimeiroTrimestreBD = 7.96
+var notaDoSegundoTrimestreBD = 9
+//
+var notaFinalBD = (notaDoPrimeiroTrimestreBD + notaDoSegundoTrimestreBD) / 2
+var notaFixadaBD = notaFinalBD.toFixed(2)
+
+// Notas de DESENHO AUXILIADO POR COMPUTADOR I
+var notaDoPrimeiroTrimestreCADI = 7.50
+var notaDoSegundoTrimestreCADI = 7
+//
+var notaFinalCADI = (notaDoPrimeiroTrimestreCADI + notaDoSegundoTrimestreCADI) / 2
+var notaFixada = notaFinalCADI.toFixed(2)
+
+// Notas de FOTOINTERPRETAÇÃO A
+var notaDoPrimeiroTrimestreFOTO = 7.04
+var notaDoSegundoTrimestreFOTO = 8.42
+//
+var notaFinalFOTO = (notaDoPrimeiroTrimestreFOTO + notaDoSegundoTrimestreFOTO) / 2
+var notaFixadaFOTO = notaFinalFOTO.toFixed(2)
+
+// Notas de GEOESTATÍSTICA A 
+var notaDoPrimeiroTrimestreGEOESTA = 10.00
+var notaDoSegundoTrimestreGEOESTA = 9.80
+//
+var notaFinalGEOESTA = (notaDoPrimeiroTrimestreGEOESTA + notaDoSegundoTrimestreGEOESTA) / 2
+var notaFixada = notaFinalGEOESTA.toFixed(2)
+
+// Notas de GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS
+var notaDoPrimeiroTrimestreGARH = 5.90
+var notaDoSegundoTrimestreGARH = 9.50
+//
+var notaFinalGARH = (notaDoPrimeiroTrimestreGARH + notaDoSegundoTrimestreGARH) / 2
+var notaFixadaGARH = notaFinalGARH.toFixed(2)
+
+// Notas de PLANEJAMENTO E PROJETOS ADMINISTRATIVOS
+var notaDoPrimeiroTrimestrePPA = 8.30
+var notaDoSegundoTrimestrePPA = 6.81
+//
+var notaFinalPPA = (notaDoPrimeiroTrimestrePPA + notaDoSegundoTrimestrePPA) / 2
+var notaFixadaPPA = notaFinalPPA.toFixed(2)
+
+// Notas de PROCESSAMENTO DIGITAL DE IMAGENS A
+var notaDoPrimeiroTrimestrePDI = 7.22
+var notaDoSegundoTrimestrePDI = 9.44
+//
+var notaFinalPDI = (notaDoPrimeiroTrimestrePDI + notaDoSegundoTrimestrePDI) / 2
+var notaFixadaPDI = notaFinalPDI.toFixed(2)
+
+//Notas de BANCO DE DADOS ESPACIAIS II
+console.log("Bem vindo, " + nome)
+console.log("BANCO DE DADOS ESPACIAIS II - Nota Final: " + notaFinalBD)
+//Notas de DESENHO AUXILIADO POR COMPUTADOR I
+console.log("DESENHO AUXILIADO POR COMPUTADOR I - Nota Final: " + notaFinalCADI)
+//Notas de FOTOINTERPRETAÇÃO A
+console.log("FOTOINTERPRETAÇÃO A - Nota Final: " + notaFinalFOTO)
+//Notas de GEOESTATÍSTICA A 
+console.log("Notas de GEOESTATÍSTICA A  - Nota Final: " + notaFinalGEOESTA)
+//Notas de GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS
+console.log("GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS - Nota Final: " + notaFinalGARH)
+//Notas de PLANEJAMENTO E PROJETOS ADMINISTRATIVOS
+console.log("PLANEJAMENTO E PROJETOS ADMINISTRATIVOS - Nota Final: " + notaFinalPPA)
+//Notas de PROCESSAMENTO DIGITAL DE IMAGENS A
+console.log("PROCESSAMENTO DIGITAL DE IMAGENS A - Nota Final: " + notaFinalPDI)
+
+
