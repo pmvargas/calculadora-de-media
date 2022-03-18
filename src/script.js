9b15c7df6758570fe92580f7830c57d90e317049
Paula Vargas <43431963+pmvargas@users.noreply.github.com>
2022-03-17 21:13:51 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,68 +1,68 @@
 var nome ="Quadro de Notas da aluna Paula Vargas do Curso Superior de Tecnologia de Geoprocessamento"
 
 // Notas de BANCO DE DADOS ESPACIAIS II
-var notaDoPrimeiroTrimestreBD = 7.96
-var notaDoSegundoTrimestreBD = 9
+var PrimeiraNotaBD = 7.96
+var SegundaNotaBD = 9
 //
-var notaFinalBD = (notaDoPrimeiroTrimestreBD + notaDoSegundoTrimestreBD) / 2
+var notaFinalBD = (PrimeiraNotaBD + SegundaNotaBD) / 2
 var notaFixadaBD = notaFinalBD.toFixed(2)
 
 // Notas de DESENHO AUXILIADO POR COMPUTADOR I
-var notaDoPrimeiroTrimestreCADI = 7.50
-var notaDoSegundoTrimestreCADI = 7
+var PrimeiraNotaCADI = 7.50
+var SegundaNotaCADI = 7
 //
-var notaFinalCADI = (notaDoPrimeiroTrimestreCADI + notaDoSegundoTrimestreCADI) / 2
-var notaFixada = notaFinalCADI.toFixed(2)
+var notaFinalCADI = (PrimeiraNotaCADI + SegundaNotaCADI) / 2
+var notaFixadaCADI = notaFinalCADI.toFixed(2)
 
 // Notas de FOTOINTERPRETAÇÃO A
-var notaDoPrimeiroTrimestreFOTO = 7.04
-var notaDoSegundoTrimestreFOTO = 8.42
+var PrimeiraNotaFOTO = 7.04
+var SegundaNotaFOTO = 8.42
 //
-var notaFinalFOTO = (notaDoPrimeiroTrimestreFOTO + notaDoSegundoTrimestreFOTO) / 2
+var notaFinalFOTO = (PrimeiraNotaFOTO + SegundaNotaFOTO) / 2
 var notaFixadaFOTO = notaFinalFOTO.toFixed(2)
 
 // Notas de GEOESTATÍSTICA A 
-var notaDoPrimeiroTrimestreGEOESTA = 10.00
-var notaDoSegundoTrimestreGEOESTA = 9.80
+var PrimeiraNotaGEOESTA = 10.00
+var SegundaNotaGEOESTA = 9.80
 //
-var notaFinalGEOESTA = (notaDoPrimeiroTrimestreGEOESTA + notaDoSegundoTrimestreGEOESTA) / 2
-var notaFixada = notaFinalGEOESTA.toFixed(2)
+var notaFinalGEOESTA = (PrimeiraNotaGEOESTA + SegundaNotaGEOESTA) / 2
+var notaFixadaGEOESTA = notaFinalGEOESTA.toFixed(2)
 
 // Notas de GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS
-var notaDoPrimeiroTrimestreGARH = 5.90
-var notaDoSegundoTrimestreGARH = 9.50
+var PrimeiraNotaGARH = 5.90
+var SegundaNotaGARH = 9.50
 //
-var notaFinalGARH = (notaDoPrimeiroTrimestreGARH + notaDoSegundoTrimestreGARH) / 2
+var notaFinalGARH = (PrimeiraNotaGARH + SegundaNotaGARH) / 2
 var notaFixadaGARH = notaFinalGARH.toFixed(2)
 
 // Notas de PLANEJAMENTO E PROJETOS ADMINISTRATIVOS
-var notaDoPrimeiroTrimestrePPA = 8.30
-var notaDoSegundoTrimestrePPA = 6.81
+var PrimeiraNotaPPA = 8.30
+var SegundaNotaPPA = 6.81
 //
-var notaFinalPPA = (notaDoPrimeiroTrimestrePPA + notaDoSegundoTrimestrePPA) / 2
+var notaFinalPPA = (PrimeiraNotaPPA + SegundaNotaPPA)/2
 var notaFixadaPPA = notaFinalPPA.toFixed(2)
 
 // Notas de PROCESSAMENTO DIGITAL DE IMAGENS A
-var notaDoPrimeiroTrimestrePDI = 7.22
-var notaDoSegundoTrimestrePDI = 9.44
+var PrimeiraNotaPDI = 7.22
+var SegundaNotaPDI = 9.44
 //
-var notaFinalPDI = (notaDoPrimeiroTrimestrePDI + notaDoSegundoTrimestrePDI) / 2
+var notaFinalPDI = (PrimeiraNotaPDI + SegundaNotaPDI)/2
 var notaFixadaPDI = notaFinalPDI.toFixed(2)
 
 //Notas de BANCO DE DADOS ESPACIAIS II
 console.log("Bem vindo, " + nome)
-console.log("BANCO DE DADOS ESPACIAIS II - Nota Final: " + notaFinalBD)
+console.log("BANCO DE DADOS ESPACIAIS II - Nota Final: " + notaFixadaBD)
 //Notas de DESENHO AUXILIADO POR COMPUTADOR I
-console.log("DESENHO AUXILIADO POR COMPUTADOR I - Nota Final: " + notaFinalCADI)
+console.log("DESENHO AUXILIADO POR COMPUTADOR I - Nota Final: " + notaFixadaCADI)
 //Notas de FOTOINTERPRETAÇÃO A
-console.log("FOTOINTERPRETAÇÃO A - Nota Final: " + notaFinalFOTO)
+console.log("FOTOINTERPRETAÇÃO A - Nota Final: " + notaFixadaFOTO)
 //Notas de GEOESTATÍSTICA A 
-console.log("Notas de GEOESTATÍSTICA A  - Nota Final: " + notaFinalGEOESTA)
+console.log("Notas de GEOESTATÍSTICA A  - Nota Final: " + notaFixadaGEOESTA)
 //Notas de GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS
-console.log("GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS - Nota Final: " + notaFinalGARH)
+console.log("GEOTECNOLOGIAS APLICADAS AOS RECURSOS HÍDRICOS - Nota Final: " + notaFixadaGARH)
 //Notas de PLANEJAMENTO E PROJETOS ADMINISTRATIVOS
-console.log("PLANEJAMENTO E PROJETOS ADMINISTRATIVOS - Nota Final: " + notaFinalPPA)
+console.log("PLANEJAMENTO E PROJETOS ADMINISTRATIVOS - Nota Final: " + notaFixadaPPA)
 //Notas de PROCESSAMENTO DIGITAL DE IMAGENS A
-console.log("PROCESSAMENTO DIGITAL DE IMAGENS A - Nota Final: " + notaFinalPDI)
+console.log("PROCESSAMENTO DIGITAL DE IMAGENS A - Nota Final: " + notaFixadaPDI)
 
 
